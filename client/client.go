7ac8bb69b4583e55e3493703aa40414babf5b865
NemoFlip <artgamer3163@gmail.com>
2024-08-01 12:09:59 +0300

    made: client and server for the app. done: homework.

--- client/client.go
+++ client/client.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io"
+	"net/http"
+)
 
+func main() {
+	resp, err := http.Get("http://127.0.0.1:8080")
+	if err != nil {
+		fmt.Printf("error sending get request: %s", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("error reading response: %s", err.Error())
+		return
+	}
+	if string(data) == "Ok" {
+		fmt.Println("GET request was successful!")
+	} else {
+		fmt.Println("Unknown response word.")
+	}
 }
--- server/server.go
+++ server/server.go
@@ -3,13 +3,19 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 )
 
 func main() {
 	router := gin.Default()
 
+	router.GET("/", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Ok")
+	})
+
 	err := router.Run("127.0.0.1:8080")
 	if err != nil {
 		log.Printf("Error of listening and serving: %s", err.Error())
 	}
+
 }
